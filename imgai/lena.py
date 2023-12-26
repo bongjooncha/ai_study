cb4aa3eabfbd72537d353cac8f49962f7bad5cb2
hiddenbebb <cbj4133@naver.com>
2023-12-26 23:55:20 +0900

    d

--- imgai/lena.py
+++ imgai/lena.py
@@ -8,5 +8,5 @@ img_gray = cv2.imread('./lena.jpg', cv2.IMREAD_GRAYSCALE)
 # //이미지 행렬차원 확인
 print(type(img_color),img_color.shape)
 
-img_resized = cv2.resize(img_color, (512,512))
-cv2.imwrite('d.img',img_resized)
+img_resized = cv2.resize(img_color, (100,100))
+cv2.imwrite('./d.jpg',img_resized)
